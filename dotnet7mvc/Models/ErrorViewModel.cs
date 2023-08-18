97fcc188e14b7e7d4ec07f7c20021470448c79ab
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-08-18 12:56:15 +0100

    Add project files.

---
+++ dotnet7mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet7mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
